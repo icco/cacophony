8a992a90947e0f80ba55c691b4a3005382d1e51d
Nat Welch <nat@natwelch.com>
2017-11-26 19:19:15 +0000

    Stubbing out some db code

---
+++ models/url.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+)
+
+type SavedUrl struct {
+	Id         int64
+	Link       string
+	TweetIds   []string
+	CreatedAt  time.Time
+	ModifiedAt time.Time
+}
+
+func AllSavedUrls() ([]*SavedUrls, error) {
+	rows, err := db.Query("SELECT * FROM saved_urls")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	urls := make([]*SavedUrl, 0)
+	for rows.Next() {
+		url := new(SavedUrl)
+		err := rows.Scan()
+		if err != nil {
+			return nil, err
+		}
+		urls = append(urls, url)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return urls, nil
+}
