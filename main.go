6f440466aaef2a0ee4f8c3e84c6dc619c5eb8f76
Nat Welch <nat@natwelch.com>
2022-11-25 22:24:42 +0000

    move twitter

--- main.go
+++ main.go
@@ -3,21 +3,14 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
-	"github.com/coreos/pkg/flagutil"
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/icco/cacophony/models"
-	"github.com/icco/cron/shared"
-	"github.com/icco/cron/tweets"
 	"github.com/icco/gutil/logging"
 	"github.com/icco/gutil/otel"
 	"go.uber.org/zap"
@@ -92,85 +85,12 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func cronHandler(w http.ResponseWriter, r *http.Request) {
-	flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
-	consumerKey := flags.String("consumer-key", "", "Twitter Consumer Key")
-	consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
-	accessToken := flags.String("access-token", "", "Twitter Access Token")
-	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
-	flags.Parse(os.Args[1:])
-	flagutil.SetFlagsFromEnv(flags, "TWITTER")
-
 	ctx := r.Context()
 
-	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
-		log.Fatal("Consumer key/secret and Access token/secret required")
-	}
-
-	config := oauth1.NewConfig(*consumerKey, *consumerSecret)
-	token := oauth1.NewToken(*accessToken, *accessSecret)
-	// OAuth1 http.Client will automatically authorize Requests
-	httpClient := config.Client(ctx, token)
-
-	// Twitter client
-	client := twitter.NewClient(httpClient)
-
-	// Verify Credentials
-	verifyParams := &twitter.AccountVerifyParams{
-		SkipStatus:   twitter.Bool(true),
-		IncludeEmail: twitter.Bool(true),
-	}
-	user, resp, err := client.Accounts.VerifyCredentials(verifyParams)
-	if err != nil {
-		log.Errorw("Error verifying creds", "response", resp, zap.Error(err))
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	log.Debugf("User: %+v", user.ScreenName)
-
-	// Home Timeline
-	homeTimelineParams := &twitter.HomeTimelineParams{
-		Count:           200,
-		IncludeEntities: twitter.Bool(true),
-	}
-	homeTweets, resp, err := client.Timelines.HomeTimeline(homeTimelineParams)
-	if resp.Header.Get("X-Rate-Limit-Remaining") == "0" {
-		i, err := strconv.ParseInt(resp.Header.Get("X-Rate-Limit-Reset"), 10, 64)
-		if err != nil {
-			log.Errorw("Error converting int", zap.Error(err))
-		}
-		tm := time.Unix(i, 0)
-		rtlimit := fmt.Errorf("out of Rate Limit. Returns: %+v", tm)
-		http.Error(w, rtlimit.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if err != nil {
+	if err := twitterCronWorker(ctx); err != nil {
 		log.Errorw("Error getting tweets", "response", resp, zap.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
-
-	c := tweets.Twitter{
-		Config: shared.Config{
-			Log: log,
-		},
-		GraphQLToken: os.Getenv("GQL_TOKEN"),
-	}
-
-	for _, t := range homeTweets {
-		// Save tweet to DB via graphql
-		if err := c.UploadTweet(ctx, t); err != nil {
-			log.Errorw("problem uploading tweet", zap.Error(err))
-		}
-
-		for _, u := range t.Entities.Urls {
-			err = models.SaveURL(ctx, u.ExpandedURL, t.IDStr)
-			if err != nil {
-				log.Errorw("Error saving url", zap.Error(err))
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
 	}
 
 	w.Header().Set("Cache-Control", "no-cache")
---
+++ twitter.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/coreos/pkg/flagutil"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/icco/cacophony/models"
+	"github.com/icco/cron/shared"
+	"github.com/icco/cron/tweets"
+)
+
+func twitterCronWorker(ctx context.Context) error {
+	flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
+	consumerKey := flags.String("consumer-key", "", "Twitter Consumer Key")
+	consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
+	accessToken := flags.String("access-token", "", "Twitter Access Token")
+	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
+	flags.Parse(os.Args[1:])
+	flagutil.SetFlagsFromEnv(flags, "TWITTER")
+
+	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
+		return fmt.Errorf("Consumer key/secret and Access token/secret required")
+	}
+
+	config := oauth1.NewConfig(*consumerKey, *consumerSecret)
+	token := oauth1.NewToken(*accessToken, *accessSecret)
+	// OAuth1 http.Client will automatically authorize Requests
+	httpClient := config.Client(ctx, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// Verify Credentials
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+	user, resp, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		return err
+	}
+	log.Debugf("User: %+v", user.ScreenName)
+
+	// Home Timeline
+	homeTimelineParams := &twitter.HomeTimelineParams{
+		Count:           200,
+		IncludeEntities: twitter.Bool(true),
+	}
+	homeTweets, resp, err := client.Timelines.HomeTimeline(homeTimelineParams)
+	if resp.Header.Get("X-Rate-Limit-Remaining") == "0" {
+		i, err := strconv.ParseInt(resp.Header.Get("X-Rate-Limit-Reset"), 10, 64)
+		if err != nil {
+			return err
+		}
+		tm := time.Unix(i, 0)
+		return fmt.Errorf("out of Rate Limit. Returns: %+v", tm)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	c := tweets.Twitter{
+		Config: shared.Config{
+			Log: log,
+		},
+		GraphQLToken: os.Getenv("GQL_TOKEN"),
+	}
+
+	for _, t := range homeTweets {
+		// Save tweet to DB via graphql
+		if err := c.UploadTweet(ctx, t); err != nil {
+			return fmt.Errorf("problem uploading tweet: %w", err)
+		}
+
+		for _, u := range t.Entities.Urls {
+			err = models.SaveURL(ctx, u.ExpandedURL, t.IDStr)
+			if err != nil {
+				return fmt.Errorf("Error saving url: %w", err)
+			}
+		}
+
+		return nil
+	}
+
+}
